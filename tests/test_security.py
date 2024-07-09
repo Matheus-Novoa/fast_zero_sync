3b4b7525c597e362663f7279ed6f66375a957f4b
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-07-09 08:20:39 -0300

    Completando a cobertura de testes do token

--- tests/test_security.py
+++ tests/test_security.py
@@ -22,3 +22,25 @@ def test_jwt_invalid_token(client):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_user_credentials_exception(user, client):
+    token = create_access_token(data={'sub': "wrong_email@mail.com"})
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_username_credentials_exception(user, client):
+    token = create_access_token(data={'test': 'test'})
+    response = client.delete(
+        f'/users/{user.id}+1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
